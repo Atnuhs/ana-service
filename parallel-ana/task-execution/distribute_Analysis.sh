6f5d3e12822cd8243ff1b41043195185b11f819e
Authns <color59abe@gmail.com>
2021-06-28 18:20:32 +0900

    exeのpathを変更

--- parallel-ana/task-execution/distribute_Analysis.sh
+++ parallel-ana/task-execution/distribute_Analysis.sh
@@ -5,7 +5,7 @@ readonly DIR_SCRIPT_ROOT="$(cd "$(dirname $0)"&&pwd)"
 readonly DIR_SERVICE_ROOT="$(cd "$(dirname $0)/../../"&&pwd)"
 
 readonly DIR_RUN_ANA="${DIR_SERVICE_ROOT:?}/run-ana"
-readonly DIR_EXE="${DIR_RUN_ANA:?}/exe"
+readonly DIR_EXE="${DIR_RUN_ANA:?}/build/src"
 readonly DIR_EXEC_SCRIPT="${DIR_RUN_ANA:?}/exec-script"
 readonly FILE_EXE_BUILD="${DIR_RUN_ANA:?}/cmake_build.sh"
 readonly FILE_ALL_TASK="${DIR_SCRIPT_ROOT:?}/../task-generate/list_analysis.txt"
@@ -26,5 +26,5 @@ do
     echo "${dir_analysis:?} => $([ -d "${dir_analysis:?}" ] && echo 'T' || echo 'F')"
     mkdir -p "${dir_analysis:?}"/{exe,script}
     rsync -ah --delete "${DIR_EXE:?}"/ "${dir_analysis:?}/exe"
-    rsync -ah --delete "${DIR_EXEC_SCRIPT:?}"/ "${dir_analysis:?}/script"
+    rsync -ah --delete "${DIR_EXEC_SCRIPT:?}"/ "${dir_analysis:?}/exe-script"
 done < "${FILE_ALL_TASK:?}"
