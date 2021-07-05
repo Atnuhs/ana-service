137548b7f9cea2ea97dd07eb1006b1bfe5bde5f5
Authns <color59abe@gmail.com>
2021-07-05 17:48:26 +0900

    ANA-SERVICEディレクトリ再構成

---
+++ analysis-by-run/script/distribute_Analysis.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+readonly DIR_ROOT="$(cd "$(dirname $0)/../../.."&&pwd)"
+readonly DIR_ANA_SER="$(cd "$(dirname $0)/.."&&pwd)"
+readonly DIR_ANALYSIS_BY_RUN="$(cd "$(dirname $0)/.."&&pwd)"
+
+readonly DIR_PACKAGE="${DIR_ANALYSIS_BY_RUN:?}/package"
+readonly DIR_TASK=""
+readonly FILE_ALL_TASK="${DIR_ANALYSIS_BY_RUN:?}/../task-generate/task_all.txt"
+
+
+
+echo "##### 解析用Fortranのコンパイル" && {
+"${DIR_PACKAGE:?}/cmake_build.sh"
+"${DIR_PACKAGE:?}/make_dist.sh"
+}
+
+
+echo "##### 解析スクリプトの配布 #####"
+while read dir_analysis
+do
+    echo "${dir_analysis:?} => $([ -d "${dir_analysis:?}" ] && echo 'T' || echo 'F')"
+    mkdir -p "${dir_analysis:?}"/{exe,script}
+    rsync -ah --delete "${DIR_PACKAGE}/dist/" "${dir_analysis:?}"
+done < "${FILE_ALL_TASK:?}"
