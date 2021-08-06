8ef0d7b59c419a3bb6308157a6017cac49801e28
Atnuhs <color59abe@gmail.com>
2021-08-06 13:20:32 +0900

    totyuu

--- analysis-by-run/package/dist/script/ana_thcd.sh
+++ analysis-by-run/package/dist/script/ana_thcd.sh
@@ -9,7 +9,6 @@ readonly FILENAME_RESULT="thcd"
 readonly FILE_EXE=""${DIR_ROOT}"/bin/${FILENAME_EXE}"
 readonly FILE_RESULT="${DIR_ROOT}/${FILENAME_RESULT}"
 
-echo "##### ana => ${FILENAME_EXE}"
 echo "##### dir => ${DIR_ROOT}"
 mkdir -p "${FILE_RESULT}"
 cd "${DIR_ROOT}"
--- analysis-by-run/package/exec-script/ana_temp.sh
+++ analysis-by-run/package/exec-script/ana_temp.sh
@@ -8,7 +8,6 @@ readonly FILENAME_RESULT="temp"
 readonly FILE_EXE=""${DIR_ROOT}"/bin/${FILENAME_EXE}"
 readonly FILE_RESULT="${DIR_ROOT}/${FILENAME_RESULT}"
 
-echo "##### ana => ${FILENAME_EXE}"
 echo "##### dir => ${DIR_ROOT}"
 mkdir -p "${FILE_RESULT}"
 cd "${DIR_ROOT}"
