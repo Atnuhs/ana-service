40c671ca9d9d819b6f8ee114fad3af0432d10d9d
Atnuhs <color59abe@gmail.com>
2021-08-08 13:29:45 +0900

    ok

--- analysis-by-project/package/exec-script/lib/common.sh
+++ analysis-by-project/package/exec-script/lib/common.sh
@@ -3,8 +3,8 @@
 set -eu -o pipefail
 trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 
-readonly DIR_ANALYSIS="$(cd "$(dirname "${0}")/../.."; pwd)"
-readonly DIR_RATE="$(cd "$(dirname "${0}")/../../.."; pwd)"
+readonly DIR_ANALYSIS="$(cd "$(dirname "${0}")/.."; pwd)"
+readonly DIR_RATE="$(cd "$(dirname "${0}")/../.."; pwd)"
 
 readonly DIR_CALCULATION="${DIR_RATE}/calculation"
 
@@ -14,3 +14,10 @@ readonly FILE_STDDEV="${DIR_ANALYSIS}/script/lib/stddev.awk"
 readonly DIR_RESULT="${DIR_ANALYSIS}/${DIRNAME_RESULT}"
 
 readonly FILE_ANALYSIS_RUN="${DIR_ANALYSIS}/analysis_run.txt"
+
+split_file() {
+    sp="${1}"
+    awk "NR%${sp}==1"
+}
+
+export -f split_file
