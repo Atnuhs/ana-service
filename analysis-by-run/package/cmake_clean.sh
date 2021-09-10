5b5e79f699e2628acef2faf93e1878c640286bee
Authns <color59abe@gmail.com>
2021-09-10 16:17:24 +0900

    update

--- analysis-by-run/package/cmake_clean.sh
+++ analysis-by-run/package/cmake_clean.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -eu -o pipefail
+trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 
 readonly DIR_ROOT="$(cd "$(dirname "${0}")"; pwd)"
 
-rm -r "${DIR_ROOT:?}/build"/*
+rm -rf "${DIR_ROOT}/build"/* 2> /dev/null
+rm -f "${DIR_ROOT}/src"/*.mod 2> /dev/null
+rm -f "${DIR_ROOT}/lib"/*.mod 2> /dev/null
