57c74ae3be6b1855876cbb471de9e45b8118fdf7
Atnuhs <color59abe@gmail.com>
2021-09-16 15:00:10 +0900

    change cmake

--- analysis-by-project/package/cmake_clean.sh
+++ analysis-by-project/package/cmake_clean.sh
@@ -4,7 +4,7 @@ trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 
 readonly DIR_ROOT="$(cd "$(dirname "${0}")"; pwd)"
 
-rm -rf "${DIR_ROOT}/build"/*
+rm -rf "${DIR_ROOT}/build"
 rm -f "${DIR_ROOT}/src"/*.mod
 rm -f "${DIR_ROOT}/lib"/*.mod
 rm  -rf "${DIR_ROOT}/dist"
