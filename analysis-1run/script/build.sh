6d9fd56eccf11eceacd50bd5c73bc790f3050729
Authns <color59abe@gmail.com>
2021-10-11 16:57:52 +0900

    細かい変化

--- analysis-1run/script/build.sh
+++ analysis-1run/script/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+set -euo pipefail
+trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 
 . "$(dirname $0)/lib/common.sh"
+
 readonly DIR_FORTRAN_COMPILER='/opt/rh/devtoolset-8/root/usr/bin/gfortran' # mdz11
 # readonly DIR_FORTRAN_COMPILER='/opt/intel/compilers_and_libraries_2016.4.258/linux/bin/intel64/ifort' # wsk2
 
--- analysis-1run/script/lib/common.sh
+++ analysis-1run/script/lib/common.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eu -o pipefail
+set -euo pipefail
 trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 
 readonly DIR_ROOT="$(cd $(dirname $0)/..; pwd)"
