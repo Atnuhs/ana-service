d14254bc42234ded7529c90ab793951ba1d4f170
ohtoriken <ohtorilab@gmail.com>
2022-01-26 18:05:50 +0900

    fix filename ana aggregate

--- para-analysis/script-allrun/parallel_ana.sh
+++ para-analysis/script-allrun/parallel_ana.sh
@@ -13,8 +13,8 @@ $(dirname $0)/make_task_div.sh
 # clean_analysis # [!注意!] taskのAnalysisを全部消す
 
 # 解析
-# parallel_analysis 'temp.sh'
-# parallel_analysis 'gr.sh'
+parallel_analysis 'temp.sh'
+parallel_analysis 'gr.sh'
 # parallel_analysis 'ext_gr.sh'
 # parallel_analysis 'molecular_orientation.sh'
 # parallel_analysis 'GK_thcd.sh'
@@ -23,4 +23,4 @@ $(dirname $0)/make_task_div.sh
 # parallel_analysis 'vis.sh'
 # parallel_analysis 'msd.sh'
 # parallel_analysis 'rmsd.sh'
-parallel_analysis '3dgr_peak.sh'
+# parallel_analysis '3dgr_peak.sh'
