0f9603d7935fe2a14dfaa19ac39eefcf8b5a7255
Atnuhs <color59abe@gmail.com>
2022-01-28 01:50:31 +0900

    A

--- parallel-other-pc/calc_other_pc.sh
+++ parallel-other-pc/calc_other_pc.sh
@@ -5,12 +5,14 @@ trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 DIR_ROOT="$(cd $(dirname "$0")/../../; pwd)"
 
 readonly HOSTNAME_REMOTE="$1"
-readonly DIR_TARGET_CALC="$2"
+readonly DIR_TARGET_CALC_ROOT="$2"
+readonly RUNNUM="$3"
+readonly SUBDIR_RUN="$(printf "calculation/run%02g" "${RUNNUM}")"
 
-echo "${DIR_ROOT}"
-TMPDIR=$(ssh -n "${HOSTNAME_REMOTE}" "mktemp -d")
+echo "${SUBDIR_RUN}"
+# TMPDIR=$(ssh -n "${HOSTNAME_REMOTE}" "mktemp -d")
 
+# rsync -ahvz "${DIR_TARGET_CALC}" "${HOSTNAME_REMOTE}:${TMPDIR}"
+# rsync -ahvz "${DIR_TARGET_INPUT}" "${HOSTNAME_REMOTE}:${TMPDIR}"
 
-rsync -ahvz "${DIR_TARGET_CALC}" "${HOSTNAME_REMOTE}:${TMPDIR}"
-
-ssh -n "${HOSTNAME_REMOTE}" "rm -rvf ${TMPDIR}"
+# ssh -n "${HOSTNAME_REMOTE}" "rm -rf ${TMPDIR}"
