5cd9119add70d5c392bee24d2fe7a57dcfe2bf88
Atnuhs <color59abe@gmail.com>
2022-01-28 01:40:56 +0900

    a

--- parallel-other-pc/calc_other_pc.sh
+++ parallel-other-pc/calc_other_pc.sh
@@ -10,5 +10,7 @@ readonly DIR_TARGET_CALC="$2"
 echo "${DIR_ROOT}"
 TMPDIR=$(ssh -n "${HOSTNAME_REMOTE}" "mktemp -d")
 
+
 rsync -ahvz "${DIR_TARGET_CALC}" "${HOSTNAME_REMOTE}:${TMPDIR}"
 
+ssh -n "${HOSTNAME_REMOTE}" "rm -rvf ${TMPDIR}"
