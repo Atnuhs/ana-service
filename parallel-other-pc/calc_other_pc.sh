34d630372ca27e632929fae5c2b6b6ee47df74a9
Atnuhs <color59abe@gmail.com>
2022-01-28 01:59:29 +0900

    a

--- parallel-other-pc/calc_other_pc.sh
+++ parallel-other-pc/calc_other_pc.sh
@@ -7,12 +7,19 @@ DIR_ROOT="$(cd $(dirname "$0")/../../; pwd)"
 readonly HOSTNAME_REMOTE="$1"
 readonly DIR_TARGET_CALC_ROOT="$2"
 readonly RUNNUM="$3"
+readonly FILENAME_ANALYSIS="$4"
 readonly SUBDIR_RUN="$(printf "calculation/run%02g" "${RUNNUM}")"
 
-echo "${SUBDIR_RUN}"
-# TMPDIR=$(ssh -n "${HOSTNAME_REMOTE}" "mktemp -d")
+TMPDIR=$(ssh -n "${HOSTNAME_REMOTE}" "mktemp -d")
 
-# rsync -ahvz "${DIR_TARGET_CALC}" "${HOSTNAME_REMOTE}:${TMPDIR}"
-# rsync -ahvz "${DIR_TARGET_INPUT}" "${HOSTNAME_REMOTE}:${TMPDIR}"
+: 計算ディレクトリコピー | {
+    cd "$DIR_TARGET_CALC_ROOT"
+    rsync -ahvzR "${SUBDIR_RUN}" "${HOSTNAME_REMOTE}:${TMPDIR}"
+    rsync -ahvz "input" "${HOSTNAME_REMOTE}:${TMPDIR}"
+}
+
+: 解析 | {
+    ssh -n "${HOSTNAME_REMOTE}" "cd ${TMPDIR}; mkdir Analysis; cd Analysis; /home/user0/abe/analysis-1run/script/${FILENAME_ANALYSIS}"
+}
 
 # ssh -n "${HOSTNAME_REMOTE}" "rm -rf ${TMPDIR}"
