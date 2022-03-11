4b5c8f60f5d873a5ec9deb48c6e302d7c8de8f91
ohtoriken <ohtorilab@gmail.com>
2022-03-11 16:57:34 +0900

    rm old analysis codes

--- analysis-1run/lib/const.f90
+++
@@ -1,9 +0,0 @@
-module const_mod
-    use,intrinsic :: iso_fortran_env
-    implicit none
-    real(real64),parameter:: pi=acos(-1d0)
-    real(real64),parameter:: an=6.0221367d+23
-    real(real64),parameter:: kbt=1.380658d-23
-
-end module const_mod
-    
--- analysis-1run/script/msd.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -euo pipefail
-trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
-
-. "$(dirname $0)/lib/common.sh"
-ana "msd.out" "msd"
