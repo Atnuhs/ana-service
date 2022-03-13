104aa94009c0885ae0dca988350a651fe5128d9f
ohtoriken <ohtorilab@gmail.com>
2022-03-13 18:58:24 +0900

    add new codes to all_analysis.sh

--- analysis-mpi/script/all_analysis.sh
+++ analysis-mpi/script/all_analysis.sh
@@ -7,9 +7,13 @@ trap 'echo "ERROR: line no = $LINENO, exit status = $?" >&2; exit 1' ERR
 
 "${DIR_ROOT}/script/temp.sh"
 "${DIR_ROOT}/script/gr.sh"
+"${DIR_ROOT}/script/ext_gr.sh"
+"${DIR_ROOT}/script/molecular_orientation.sh"
 "${DIR_ROOT}/script/GK_thcd.sh"
 "${DIR_ROOT}/script/GK_viscousity.sh"
 "${DIR_ROOT}/script/thcd.sh"
 "${DIR_ROOT}/script/viscousity.sh"
 "${DIR_ROOT}/script/msd.sh"
 "${DIR_ROOT}/script/rmsd.sh"
+"${DIR_ROOT}/script/tdc.sh"
+"${DIR_ROOT}/script/rdc.sh"
