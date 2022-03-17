44eff9d5b6c6d33089bc67e37d5b52547a50d587
ohtoriken <ohtorilab@gmail.com>
2022-03-17 13:54:40 +0900

    fix thcd

--- analysis-mpi/src/thcd.f90
+++ analysis-mpi/src/thcd.f90
@@ -8,16 +8,18 @@ program main
     character(100),parameter:: file_thcd = 'thcd/thcd.txt'
     integer(int32):: ndata, fst_calc, lst_calc
     real(real64), allocatable:: x(:), integ(:), integ_stdev(:)
-    real(real64):: temp0, temp
+    real(real64):: temp0, temp, temp_adjust
     real(real64):: convergence, convergence_stdev
 
     read*, fst_calc, lst_calc
-    call load_condition_for_thcd_ana(ndata, temp)
+    call load_condition_for_thcd_ana(ndata, temp0)
     call read_x('./temp/temp_mean.txt', temp)
     allocate(x(ndata), integ(ndata), integ_stdev(ndata))
     call read_arx_ary_arysd(file_integ, ndata, x, integ, integ_stdev)
     convergence = mean(integ(fst_calc:lst_calc), stdev=convergence_stdev)
-    convergence = convergence * (temp0/temp)**0.25d0
-    convergence_stdev = convergence_stdev * (temp0/temp)**0.25d0
+    temp_adjust = (temp0/temp)**0.25d0
+    print*, temp, temp0, temp_adjust
+    convergence = convergence * temp_adjust
+    convergence_stdev = convergence_stdev * temp_adjust
     call write_x_xsd(file_thcd, convergence, convergence_stdev)
 end program
